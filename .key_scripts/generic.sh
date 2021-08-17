5057260e925fcc1c0b1b538fcd22d653f7ed315a
Vojtěch Moravec <vojtech.moravec.st@vsb.cz>
2021-08-17 17:59:31 +0200

    Update generic.sh to expand job parameters.

--- .key_scripts/generic.sh
+++ .key_scripts/generic.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-COMMAND=$1
-eval "${COMMAND}"
+USER_SCRIPT=$1
+PARAMETERS=$2
+
+# Expand parameter pairs for this script.
+case $PARAMETERS in
+    (*=*) eval $PARAMETERS;;
+esac
+
+# Source the user script here. We can't just eval or call it, because we would lose the expanded parameters.
+. "${USER_SCRIPT}"
